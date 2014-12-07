8d167d3d688b69579e3bb98c1217189eb2eecbfb
Zachary Crockett <towynlin@gmail.com>
2014-12-07 16:52:06 -0500

    Calculate version and type directly from ByteString

--- ParseCoap.hs
+++ ParseCoap.hs
@@ -5,7 +5,7 @@ import Data.Bits
 import qualified Data.ByteString.Lazy as B
 
 data CoAPMessage = CoAPMessage {
-  version     :: Int,
+  version     :: Word8,
   messageType :: CoAPMessageType,
   code        :: Int,
   messageID   :: Int,
@@ -15,20 +15,23 @@ data CoAPMessage = CoAPMessage {
 data CoAPMessageType = CON | NON | ACK | RST
   deriving (Show)
 
-makeCoAP :: Int -> CoAPMessageType -> Int -> Int -> Int -> CoAPMessage
+makeCoAP :: Word8 -> CoAPMessageType -> Int -> Int -> Int -> CoAPMessage
 makeCoAP v t c mid tok =
   CoAPMessage { version = v, messageType = t, code = c, messageID = mid, token = tok }
 
-typeFromFirstByte :: Word8 -> CoAPMessageType
-typeFromFirstByte b =
+parseCoAP :: B.ByteString -> CoAPMessage
+parseCoAP bs = makeCoAP (coapVersion bs) (coapMessageType bs) 0 0 0
+
+coapVersion :: B.ByteString -> Word8
+coapVersion bs = shiftR (B.head bs) 6
+
+coapMessageType :: B.ByteString -> CoAPMessageType
+coapMessageType bs =
   case t of 0 -> CON
             1 -> NON
             2 -> ACK
             3 -> RST
-  where t = shiftR b 4 .&. 3
-
-parseCoAP :: B.ByteString -> CoAPMessage
-parseCoAP msg = makeCoAP 1 (typeFromFirstByte . head $ B.unpack msg) 0 0 0
+  where t = shiftR (B.head bs) 4 .&. 3
 
 main :: IO ()
-main = B.getContents >>= \contents -> putStr . show $ parseCoAP contents
+main = B.getContents >>= \bs -> putStr . show $ parseCoAP bs
